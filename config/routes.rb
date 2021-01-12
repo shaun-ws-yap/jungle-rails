47271ae75417278c2d3ee98a300b1bf2bbe454f2
Shaun Yap <me@shaunyap.ca>
2021-01-12 02:33:18 +0000

    Add user auth features

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,15 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#show'
 
+  # user routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # session routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
